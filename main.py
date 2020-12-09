acf0e6ed321b667d43c2efeb8545b4ab6433bfd8
DonMins <maks000156@yandex.ru>
2020-12-09 21:18:08 +0400

    it is Ready

--- main.py
+++ main.py
@@ -3,45 +3,11 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 import sys
 import sqlite3
-import DataBaseHelper as db
 
 top = 400
 left = 400
 width = 1000
 height = 600
-
-
-class MyTableWidget(QMainWindow):
-
-    def __init__(self, parent):
-        super(QWidget, self).__init__(parent)
-        self.layout = QVBoxLayout(self)
-
-        # Initialize tab screen
-        self.tabs = QTabWidget()
-        self.tab1 = QWidget()
-        self.tab2 = QWidget()
-        self.tab3 = QWidget()
-        self.tab4 = QWidget()
-        self.tab5 = QWidget()
-        self.tabs.setFixedSize(970, 350)
-
-        # Add tabs
-        self.tabs.addTab(self.tab1, "Tab 1")
-        self.tabs.addTab(self.tab2, "Tab 2")
-        self.tabs.addTab(self.tab3, "Tab 3")
-        self.tabs.addTab(self.tab4, "Tab 4")
-        self.tabs.addTab(self.tab5, "Tab 5")
-
-        # Create first tab
-        self.tab1.layout = QVBoxLayout(self)
-        # self.pushButton1 = QPushButton("PyQt5 button")
-        # self.tab1.layout.addWidget(self.pushButton1)
-        self.tab1.setLayout(self.tab1.layout)
-
-        # Add tabs to widget
-        self.layout.addWidget(self.tabs)
-        self.setLayout(self.layout)
 
 class Window(QMainWindow):
     def __init__(self):
@@ -49,7 +15,7 @@ class Window(QMainWindow):
         self.layout = QFormLayout(self)
         self.db = 0
 
-        title = "Генератор курса валют на торговом рынке"
+        title = "Какая - то штука"
 
         self.setWindowTitle(title)
         self.setGeometry(top, left, width, height)
@@ -71,23 +37,27 @@ class Window(QMainWindow):
         self.btn1.move(12,50)
         self.btn1.resize(150,25)
         self.btn1.setFont(QFont('Arial', 7))
+        self.btn1.clicked.connect(self.getB1)
 
         self.btn2= QPushButton("bt2 (Query2)", self)
         self.btn2.move(174, 50)
         self.btn2.resize(150, 25)
         self.btn2.setFont(QFont('Arial', 7))
+        self.btn2.clicked.connect(self.getB2)
 
         self.btn3 = QPushButton("bt3 (Query3)", self)
         self.btn3.move(174+150+12, 50)
         self.btn3.resize(150, 25)
         self.btn3.setFont(QFont('Arial', 7))
+        self.btn3.clicked.connect(self.getB3)
 
         self.combo = QComboBox(self)
-        self.combo.addItems(["Ubuntu", "Mandriva",
-                        "Fedora", "Arch", "Gentoo"])
+        self.combo.addItems(['',"date", "trans",
+                        "symbol", "qty", "price"])
         self.combo.resize(150, 25)
         # self.combo.activated[str].connect(self.onActivated)
         self.combo.move(174+150+12+200, 50)
+        self.combo.currentTextChanged.connect(self.getCombo)
 
         self.tabs = QTabWidget(self)
         self.tab1 = QTableWidget (self)
@@ -114,6 +84,80 @@ class Window(QMainWindow):
             table.setItem(row, col, cell)
             col += 1
 
+    def getCombo(self, val):
+        if self.db != 0:
+            if val != '':
+                data = self.db.cursor().execute('SELECT ' + val + ' FROM stocks').fetchall()
+
+                self.tab3.setColumnCount(1)
+                columnsName = [val]
+                self.tab3.rowCount()
+                self.tab3.setHorizontalHeaderLabels(columnsName)
+
+                while (self.tab3.rowCount() > 0):
+                    self.tab3.removeRow(0)
+
+                for row in data:
+                    self.addTableRow(self.tab3, row)
+            else:
+                while (self.tab3.rowCount() > 0):
+                    self.tab3.removeRow(0)
+            self.tabs.setCurrentIndex(2)
+
+
+    def getB1(self):
+        if self.db != 0:
+            data = self.db.cursor().execute('SELECT date FROM stocks').fetchall()
+
+            self.tab2.setColumnCount(1)
+            columnsName = ['date']
+            self.tab2.rowCount()
+            self.tab2.setHorizontalHeaderLabels(columnsName)
+
+            if self.tab2.rowCount() == 0:
+                for row in data:
+                    self.addTableRow(self.tab2, row)
+            self.tabs.setCurrentIndex(1)
+
+
+    def getB2(self):
+        if self.db != 0:
+            data = self.db.cursor().execute('SELECT * FROM stocks').fetchall()
+
+            info_table = self.db.cursor().execute('PRAGMA table_info(stocks)').fetchall()
+            self.tab4.setColumnCount(len(info_table))
+            columnsName = []
+            for row in info_table:
+                columnsName.append(row[1])
+            self.tab4.rowCount()
+            self.tab4.setHorizontalHeaderLabels(columnsName)
+
+            if self.tab4.rowCount() == 0:
+                for row in data:
+                    self.addTableRow(self.tab4, row)
+
+            self.tabs.setCurrentIndex(3)
+
+    def getB3(self):
+        if self.db != 0:
+            data = self.db.cursor().execute('SELECT * FROM stocks').fetchall()
+
+            info_table = self.db.cursor().execute('PRAGMA table_info(stocks)').fetchall()
+            self.tab5.setColumnCount(len(info_table))
+            columnsName = []
+            for row in info_table:
+                columnsName.append(row[1])
+            self.tab5.rowCount()
+            self.tab5.setHorizontalHeaderLabels(columnsName)
+
+            if self.tab5.rowCount() == 0:
+                for row in data:
+                    self.addTableRow(self.tab5, row)
+
+            self.tabs.setCurrentIndex(4)
+
+
+
     def getConnectionDB(self):
         conn = sqlite3.connect('example.db')
         c = conn.cursor()
@@ -124,8 +168,10 @@ class Window(QMainWindow):
                      ('2006-04-06', 'SELL', 'IBM', 500, 53.00),
                      ('2006-01-05', 'BUY', 'RHAT', 100, 35.14)]
 
-        c.executemany('INSERT INTO stocks VALUES (?,?,?,?,?)', purchases)
-        conn.commit()
+        if (c.execute('SELECT count(*) FROM stocks').fetchall()[0][0]==0):
+            c.executemany('INSERT INTO stocks VALUES (?,?,?,?,?)', purchases)
+            conn.commit()
+
         data = c.execute('SELECT * FROM stocks').fetchall()
 
         info_table = c.execute('PRAGMA table_info(stocks)').fetchall()
@@ -136,47 +182,37 @@ class Window(QMainWindow):
         self.tab1.rowCount()
         self.tab1.setHorizontalHeaderLabels(columnsName)
 
-        for row in data:
-            self.addTableRow(self.tab1, row)
+        if self.tab1.rowCount() ==0:
+            for row in data:
+                self.addTableRow(self.tab1, row)
 
-        # cur = g.fetchall()
-        # for i, row in enumerate(cur):
-        #     for j, val in enumerate(row):
-        #         self.tabs.setItem(i, j, QTableWidgetItem(str(val)))
-        #
-        # for row in c.execute('SELECT * FROM stocks ORDER BY price'):
-        #     print(row)
+        self.tabs.setCurrentIndex(0)
         self.db = conn
 
     def closeConnection(self):
         if self.db != 0:
-            self.db.cursor().execute('delete from stocks')
-            self.db.commit()
+            while (self.tab1.rowCount() > 0):
+                self.tab1.removeRow(0)
+            self.tab1.setColumnCount(0)
+
+            while (self.tab2.rowCount() > 0):
+                self.tab2.removeRow(0)
+            self.tab2.setColumnCount(0)
+
+            while (self.tab3.rowCount() > 0):
+                self.tab3.removeRow(0)
+            self.tab3.setColumnCount(0)
+
+            while (self.tab4.rowCount() > 0):
+                self.tab4.removeRow(0)
+            self.tab4.setColumnCount(0)
+
+            while (self.tab5.rowCount() > 0):
+                self.tab5.removeRow(0)
+            self.tab5.setColumnCount(0)
 
             self.db.close()
-
-    def onActivated(self, text):
-        self.combo.setText(text)
-        self.combo.adjustSize()
-
-
-    def buttonClicked(self):
-        newOil = float(self.oilEdit.text())
-        oldOil = float(self.oil.getOil())
-        self.oil.setOil(self.oilEdit.text())
-        if newOil > oldOil:
-            self.ruble.setRuble.emit(1)
-            self.rubleEdit.setText(self.ruble.getRuble())
-
-            self.dollar.setDollar.emit(-1)
-            self.dollarEdit.setText(self.dollar.getDollar())
-
-        if newOil < oldOil:
-            self.dollar.setDollar.emit(1)
-            self.dollarEdit.setText(self.dollar.getDollar())
-
-            self.ruble.setRuble.emit(-1)
-            self.rubleEdit.setText(self.ruble.getRuble())
+            self.db = 0
 
 
 if __name__ == "__main__":
